b57b6b73fd3715fcd031d984c78d8327e3eef78d
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-05-28 21:03:21 +0400

    binary tree: search

--- src/data_structures/binary_tree.ts
+++ src/data_structures/binary_tree.ts
@@ -2,8 +2,20 @@ export class BinaryTree<T> {
   values: T[] = [];
   length: number = 0;
 
+  // O(1)
   append(value: T) {
-    this.values[this.length] = value;
+    this.values.push(value);
     ++this.length;
+  }
+
+  // O(n)
+  search(value: T) {
+    for (let i = 0; i < this.length; ++i) {
+      if (value === this.values[i]) {
+        return i;
+      }
+
+      return -1;
+    }
   }
 }
