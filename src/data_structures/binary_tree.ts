8e0e1ca2f9c39fb7150fde92b2bc18c3ba5c0aac
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-05-28 23:20:56 +0400

    binary tree: changing underlying structure

--- src/data_structures/binary_tree.ts
+++ src/data_structures/binary_tree.ts
@@ -1,21 +1,49 @@
+export class Node<T> {
+  value: T;
+  left: Node<T> | null;
+  right: Node<T> | null;
+
+  constructor(value: T) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 export class BinaryTree<T> {
-  values: T[] = [];
-  length: number = 0;
+  rootNode: Node<T> | null
+  length: number;
+
+  constructor() {
+    this.rootNode = null;
+    this.length = 0;
+  }
 
   // O(1)
   append(value: T) {
-    this.values.push(value);
-    ++this.length;
+    if (!this.rootNode) {
+      this.rootNode = new Node(value)
+      return
+    }
+
+    this.attachNode(this.rootNode, value)
+
   }
 
   // O(n)
   search(value: T) {
-    for (let i = 0; i < this.length; ++i) {
-      if (value === this.values[i]) {
-        return i;
-      }
+  }
 
-      return -1;
+  attachNode(node: Node<T>, value: T) {
+    if (!node.left) {
+      node.left = new Node(value);
+      return
     }
+    else if (!node.right) {
+      node.right = new Node(value)
+      return
+    }
+
+    this.attachNode(node.left, value);
+    this.attachNode(node.right, value);
   }
 }
